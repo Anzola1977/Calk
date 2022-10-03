e6a17f735d72db694cce53a6307d46dc7fe6a0d2
Andrew Zola <and.zola.77@gmail.com>
2022-10-03 18:07:55 +0200

    not yet finished

--- src/Calk.java
+++ src/Calk.java
@@ -8,7 +8,7 @@ public class Calk {
         double val1;
         double val2;
         char operand;
-        char calculatorOperator = 0;
+        char calculatorOperator = 'c';
         double result = 0;
 
         do {
@@ -24,7 +24,7 @@ public class Calk {
             System.out.println("Input val2: ");
             val2 = scanner.nextDouble();
             result = calculate(val1, val2, operand);
-            System.out.println(val1 + " " + operand + " " + val2 + "=" + result);
+            System.out.println(val1 + " " + operand + " " + val2 + " = " + result);
             calculatorOperator = scanner.next().charAt(0);
         } while (calculatorOperator != 's');
     }
